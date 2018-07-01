661ecc3845f2096fe8e9eac538132b2d43f0c312
Victor Alexiev <vic.alexiev@gmail.com>
2018-07-01 22:51:55 +0300

    truffle test coverage added

--- Blockchain Dev Course/Homework Assignments/19. Truffle and Ganache/03. TestContractWithTruffle/contracts/Funding.sol
+++ Blockchain Dev Course/Homework Assignments/19. Truffle and Ganache/03. TestContractWithTruffle/contracts/Funding.sol
@@ -3,81 +3,65 @@
 // contracts/Funding.sol
 pragma solidity ^0.4.19;
 
-// import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
-// import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "zeppelin-solidity/contracts/math/SafeMath.sol";
 
-contract Funding {
-    address public owner;
+contract Funding is Ownable {
+    using SafeMath for uint;
+
     uint public raised;
+    uint public goal;
+    uint public finishesAt;
     mapping(address => uint) public balances;
 
-    constructor() public {
-        owner = msg.sender;
+    modifier onlyNotFinished() {
+        require(!isFinished());
+        _;
     }
 
-    function donate() public payable {
-        balances[msg.sender] += msg.value;
-        raised += msg.value;
+    modifier onlyFinished() {
+        require(isFinished());
+        _;
+    }
+
+    modifier onlyNotFunded() {
+        require(!isFunded());
+        _;
+    }
+
+    modifier onlyFunded() {
+        require(isFunded());
+        _;
+    }
+
+    function () public payable {}
+
+    constructor(uint _duration, uint _goal) public {
+        finishesAt = now + _duration;
+        goal = _goal;
+    }
+
+    function isFinished() public view returns (bool) {
+        return finishesAt <= now;
+    }
+
+    function isFunded() public view returns (bool) {
+        return raised >= goal;
+    }
+
+    function donate() public onlyNotFinished payable {
+        balances[msg.sender] = balances[msg.sender].add(msg.value);
+        raised = raised.add(msg.value);
+    }
+
+    function withdraw() public onlyOwner onlyFunded {
+        owner.transfer(address(this).balance);
+    }
+
+    function refund() public onlyFinished onlyNotFunded {
+        uint amount = balances[msg.sender];
+        require(amount > 0);
+        balances[msg.sender] = 0;
+        msg.sender.transfer(amount);
     }
 }
-/*
-contract Funding is Ownable {
-  using SafeMath for uint;
-
-  uint public raised;
-  uint public goal;
-  uint public finishesAt;
-  mapping(address => uint) public balances;
-
-  modifier onlyNotFinished() {
-    require(!isFinished());
-    _;
-  }
-
-  modifier onlyFinished() {
-    require(isFinished());
-    _;
-  }
-
-  modifier onlyNotFunded() {
-    require(!isFunded());
-    _;
-  }
-
-  modifier onlyFunded() {
-    require(isFunded());
-    _;
-  }
-
-  function () public payable {}
-
-  function Funding(uint _duration, uint _goal) public {
-    finishesAt = now + _duration;
-    goal = _goal;
-  }
-
-  function isFinished() public view returns (bool) {
-    return finishesAt <= now;
-  }
-
-  function isFunded() public view returns (bool) {
-    return raised >= goal;
-  }
-
-  function donate() public onlyNotFinished payable {
-    balances[msg.sender] = balances[msg.sender].add(msg.value);
-    raised = raised.add(msg.value);
-  }
-
-  function withdraw() public onlyOwner onlyFunded {
-    owner.transfer(address(this).balance);
-  }
-
-  function refund() public onlyFinished onlyNotFunded {
-    uint amount = balances[msg.sender];
-    require(amount > 0);
-    balances[msg.sender] = 0;
-    msg.sender.transfer(amount);
-  }
-}
-*/
--- Blockchain Dev Course/Homework Assignments/19. Truffle and Ganache/03. TestContractWithTruffle/truffle-config.js
+++ Blockchain Dev Course/Homework Assignments/19. Truffle and Ganache/03. TestContractWithTruffle/truffle-config.js
@@ -13,13 +13,20 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  networks: {
-      development: {
-          host: "127.0.0.1",
-          port: 8545,
-          network_id: "*" // matches any network id
-      }
-  }
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "*" // matches any network id
+        },
+        coverage: {
+            host: "127.0.0.1",
+            network_id: "*",
+            port: 8555,
+            gas: 0xfffffffffff,
+            gasPrice: 0x01
+        }
+    }
 };
